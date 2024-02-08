1180a23d95f2530eb7cef6f1db0cb04f0f8c4b0d
Natalye <cavalieridev@gmail.com>
2024-02-08 18:11:29 +0100

    create onboarding state and function

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,13 @@ import logo from "./assets/logo-nlw-expert.svg"
 import { NewNoteCard } from "./components/new-note-card"
 import { NoteCard } from "./components/note-card"
 
+
+const note = {
+  date: new Date(),
+  content: 'Hello World'
+
+}
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -19,12 +26,8 @@ export function App() {
 
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard />
-        <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
       </div>
-      
     </div>
   )
 }
